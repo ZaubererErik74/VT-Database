e6c4dcae205000ec488bd168480fa4e63dac20e4
Erik Bauer <erik_bauer@icloud.com>
2026-04-22 17:57:06 +0200

    Added MainWindow JFrame Class

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,9 @@
-public class MainWindow {
-    
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+    MainWindow(){
+        this.setTitle("Main Window");
+        this.setSize(800, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
